6fcb89bd20669ae7be0f149919b0fbab766b9ee1
MasterProgrammer <yoelreyes54@gmail.com>
2022-03-31 03:26:52 +0200

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,32 @@
 <template>
-<!-- default -->
- <v-div>
-   <!-- banner de seguridad -->
-   <v-banner two-line 
-   v-model="bannerSeguridad">
-    <v-avatar
-      slot="icon"
-      color="deep-purple accent-4"
-      size="40"
-    >
-      <v-icon
-        icon="mdi-lock"
-        color="white"
-      >
-        mdi-lock
-      </v-icon>
-    </v-avatar>
+  <!-- default -->
+  <v-div>
+    <!-- banner de seguridad -->
+    <v-banner two-line v-model="bannerSeguridad">
+      <v-avatar slot="icon" color="deep-purple accent-4" size="40">
+        <v-icon icon="mdi-lock" color="white"> mdi-lock </v-icon>
+      </v-avatar>
 
-    Nuestra plataforma esta protejida contra rastreo web.
+      Nuestra plataforma esta protejida contra rastreo web.
 
-    <template v-slot:actions>
-      <v-btn
-        text
-        color="deep-purple accent-2"
-        @click="bannerSeguridad = false"
-      >
-        Cerrar
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-2"
-      >
-        Mas detalles
-      </v-btn>
+      <template v-slot:actions>
+        <v-btn
+          text
+          color="deep-purple accent-2"
+          @click="bannerSeguridad = false"
+        >
+          Cerrar
+        </v-btn>
+        <v-btn text color="deep-purple accent-2"> Mas detalles </v-btn>
+      </template>
+    </v-banner>
+    <!-- breadcrumbs -->
+    <template color="deep-purple accent-2">
+      <div mt-5>
+        <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
+      </div>
     </template>
-  </v-banner>
- </v-div>
+  </v-div>
 </template>
 
 <script>
@@ -43,11 +34,16 @@ export default {
   data() {
     return {
       bannerSeguridad: true,
+      items: [
+        {
+          text: "inicio",
+          disabled: false,
+          href: "#",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
