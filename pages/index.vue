c4c2aa1a9ed6fc4d6528658f35866f626df8a2d5
MasterProgrammer <yoelreyes54@gmail.com>
2022-03-31 03:46:15 +0200

    add carousel

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,19 @@
         <v-btn text color="deep-purple accent-2"> Mas detalles </v-btn>
       </template>
     </v-banner>
-    <!-- breadcrumbs -->
+    
+
+<!-- carousel -->
+    <template>
+  <v-carousel :show-arrows="false">
+    <v-carousel-item
+      v-for="(item,i) in imagenCarousel"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+<!-- breadcrumbs -->
     <template color="deep-purple accent-2">
       <div mt-5>
         <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
@@ -41,6 +53,20 @@ export default {
           href: "#",
         },
       ],
+       imagenCarousel: [
+          {
+            src: 'https://m.media-amazon.com/images/I/712pM1KqGhL._AC_SX466_.jpg',
+          },
+          {
+            src: 'https://www.kindpng.com/picc/m/220-2208379_celular-desarmado-siendo-reparado-con-tarjeta-de-cmara.png',
+          },
+          {
+            src: 'https://kyscomunicaciones.com/wp-content/uploads/2021/08/K41S-WEB-1.jpg',
+          },
+          {
+            src: 'https://i.linio.com/p/c15ebf71725a73a7751f5cd0c3eedd01-product.jpg',
+          },
+        ]
     };
   },
 };
